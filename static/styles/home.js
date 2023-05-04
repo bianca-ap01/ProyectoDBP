b1a0bdb566d7d13aa58d77bcbaac56c8b89a9a85
Bianca Aguinaga <bianca.aguinaga@utec.edu.pe>
2023-05-04 18:03:01 -0500

    Implementación de navigation bar

---
+++ static/styles/home.js
@@ -0,0 +1,24 @@
+const nav = document.querySelector(".nav"),
+  navOpenBtn = document.querySelector(".navOpenBtn"),
+  navCloseBtn = document.querySelector(".navCloseBtn");
+
+navOpenBtn.addEventListener("click", () => {
+  nav.classList.add("openNav");
+  searchIcon.classList.replace("uil-times", "uil-search");
+});
+
+navCloseBtn.addEventListener("click", () => {
+  nav.classList.remove("openNav");
+});
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
