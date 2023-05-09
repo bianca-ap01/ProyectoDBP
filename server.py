2ee689e3b1f0c44a6126087b9be6a00cdc367683
Bianca Aguinaga <bianca.aguinaga@utec.edu.pe>
2023-05-09 01:55:49 -0500

    Actualización de modelos y tablas

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import (
     Flask,
-    jsonify, 
-    render_template, 
+    jsonify,
+    render_template,
     request,
     abort,
     redirect,
@@ -35,21 +35,25 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # Models
 with app.app_context():
-   #Convertir el modelo en una tabla
-   db.create_all()
+    # Convertir el modelo en una tabla
+    db.create_all()
+
 
 class Usuario(db.Model):
     __tablename__ = 'usuarios'
-    id = db.Column(db.String(36), nullable=False, default=lambda:str(uuid.uuid4()), primary_key=True)
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
     name = db.Column(db.String(), nullable=False)
     lastname = db.Column(db.String(), nullable=False)
     nickname = db.Column(db.String(30), nullable=True)
     codeforces_handle = db.Column(db.String(30), nullable=True)
     atcoder_handle = db.Column(db.String(30), nullable=True)
     vjudge_handle = db.Column(db.String(30), nullable=True)
-    fecha_de_nacimiento = db.Column(db.Date(), nullable = True)
-    created_at = db.Column(db.DateTime(timezone=True), nullable = False, server_default=db.text("now()"))
-    modified_at = db.Column(db.DateTime(timezone=True), nullable = False, server_default=db.text("now()"))
+    fecha_de_nacimiento = db.Column(db.Date(), nullable=True)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True),
+                            nullable=False, server_default=db.text("now()"))
     miembros = db.relationship('Miembro', backref='usuarios', lazy=True)
 
     def __init__(self, name, lastname, nickname, fecha_de_nacimiento, codeforces_handle, atcoder_handle, vjudge_handle):
@@ -61,11 +65,11 @@ class Usuario(db.Model):
         self.atcoder_handle = atcoder_handle
         self.vjudge_handle = vjudge_handle
         self.created_at = datetime.utcnow()
-        self.modified_at =datetime.utcnow()
+        self.modified_at = datetime.utcnow()
 
     def __repr__(self):
         return f"Usuario {self.name} {self.nickname}"
-    
+
     def serialize(self):
         return {
             'id': self.id,
@@ -79,84 +83,115 @@ class Usuario(db.Model):
             'created_at': self.created_at,
             'modified_at': self.modified_at
         }
-    
-
 
 
 class Miembro(Usuario):
     __tablename__ = 'miembros'
-    user_id = db.Column(db.String(36), db.ForeignKey('usuarios.id'), nullable=False, primary_key=True)
+    __table_args__ = {'extend_existing': True}
+    user_id = db.Column(db.String(36), db.ForeignKey(
+        'usuarios.id'), nullable=False, primary_key=True)
+    miembro_directiva = db.relationship(
+        'Directiva', backref='miembros', lazy=True)
     __mapper_args__ = {
         'polymorphic_identity': 'miembro',
     }
-class NoMiembro(Usuario):
-    __tablename__ = 'no_miembros'
-    user_id = db.Column(db.String(36), db.ForeignKey('usuarios.id'), nullable=False, primary_key=True)
-    principiante = db.Column(db.Boolean, nullable=False)
-    status = db.Column(db.Boolean, nullable=False)
-    suscrito = db.Column(db.Boolean, nullable=False)
-    __mapper_args__ = {
-        'polymorphic_identity': 'no_miembro',
-    }
+
+
 class Profesor(db.Model):
     __tablename__ = 'profesores'
-    id = db.Column(db.String(36), nullable=False, default=lambda:str(uuid.uuid4()), primary_key=True)
+    __table_args__ = {'extend_existing': True}
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
     apellido = db.Column(db.String(50), nullable=False)
     correo = db.Column(db.String(100), nullable=False)
     status = db.Column(db.Boolean, nullable=False)
-    equipo_id = db.Column(db.String(36), db.ForeignKey('equipos.id'), nullable=True)
+    equipo_id = db.Column(db.String(36), db.ForeignKey(
+        'equipos.id'), nullable=False)
+
+
+profesor_equipo = db.Table('profesor_equipo',
+                           db.Column('profesor_id',  db.String(36), db.ForeignKey(
+                               'profesores.id')),
+                           db.Column('equipo_id', db.String(36), db.ForeignKey('equipos.id')))
+
 
 class Equipo(db.Model):
     __tablename__ = 'equipos'
-    id = db.Column(db.String(36), nullable=False, default=lambda:str(uuid.uuid4()), primary_key=True)
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
     nombre = db.Column(db.String(50), nullable=False)
-    profe_id = db.Column(db.String(36), db.ForeignKey('profesores.id'), nullable=False)
+    profe_id = db.Column(db.String(36), db.ForeignKey(
+        'profesores.id'), nullable=False)
     status = db.Column(db.Boolean, nullable=False)
-    profesor = db.relationship('Profesor', backref='equipos', lazy=True)
+    profesor = db.relationship(
+        'Profesor', backref='equipos', lazy=True, secondary=profesor_equipo)
+
 
 class Contest(db.Model):
     __tablename__ = 'contests'
-    id = db.Column(db.String(36), nullable=False, default=lambda:str(uuid.uuid4()), primary_key=True)
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
     cantidad_problemas = db.Column(db.Integer, nullable=False)
     privacidad = db.Column(db.Boolean, nullable=False)
     nombre = db.Column(db.String(50), nullable=False)
     duracion = db.Column(db.Integer, nullable=False)
 
+
+contest_problema = db.Table('contest_problema', db.Column('contest_id', db.String(36), db.ForeignKey(
+    'contests.id')), db.Column('problema_id', db.String(36), db.ForeignKey('problemas.id')))
+
+
 class Problema(db.Model):
     __tablename__ = 'problemas'
-    id = db.Column(db.String(36), nullable=False, default=lambda:str(uuid.uuid4()), primary_key=True)
-    contest_id = db.Column(db.String(36), db.ForeignKey('contests.id'), nullable=False)
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
+    contest_id = db.Column(db.String(36), db.ForeignKey(
+        'contests.id'), nullable=False)
     link = db.Column(db.String(100), nullable=False)
     nombre = db.Column(db.String(50), nullable=False)
     clave = db.Column(db.String(10), nullable=False)
     plataforma = db.Column(db.String(50), nullable=False)
-    contest = db.relationship('Contest', backref='problemas', lazy=True)
+    contest = db.relationship(
+        'Contest', backref='problemas', lazy=True, secondary=contest_problema)
+
 
 class Directiva(db.Model):
-    __tablename__ = 'directivas'
-    id = db.Column(db.String(36), nullable=False, default=lambda:str(uuid.uuid4()), primary_key=True)
+    __tablename__ = 'directiva'
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
     cargo = db.Column(db.String(50), nullable=False)
     fecha_salida_cargo = db.Column(db.DateTime(timezone=True), nullable=True)
     fecha_entrada_cargo = db.Column(db.DateTime(timezone=True), nullable=False)
+    member_id = db.Column(db.String(36), db.ForeignKey(
+        'miembros.user_id'), nullable=True)
 
 # Routes
+
+
 @login_manager.user_loader
 def load_user(user_id):
     return Usuario.query.get(user_id)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
+
 @app.route('/faq', methods=['GET'])
 def faq():
     return render_template('faq.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = Usuario.query.filter_by(nickname=request.form['nickname']).first()
+        user = Usuario.query.filter_by(
+            nickname=request.form['nickname']).first()
         if user:
             if user.password == request.form['password']:
                 login_user(user)
@@ -171,12 +206,14 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     flash('Has cerrado sesión correctamente')
     return redirect(url_for('home'))
+
 
 # Run the app
 if __name__ == '__main__':
