fec36341cfe6bd042f8a13032a2f6fbcc8f27539
Bianca Aguinaga <bianca.aguinaga@utec.edu.pe>
2023-05-13 18:49:00 -0500

    Modificación de tablas y nueva carpeta de migraciones

--- server.py
+++ server.py
@@ -10,7 +10,6 @@ from flask import (
     flash
 )
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.dialects.postgresql import UUID
 from flask_migrate import Migrate
 import uuid
 import os
@@ -30,7 +29,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
+app.config['UPLOAD_FOLDER'] = 'static/users'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
 # Models
@@ -39,148 +38,248 @@ with app.app_context():
     db.create_all()
 
 
-class Usuario(db.Model):
-    __tablename__ = 'usuarios'
+class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.String(36), nullable=False,
                    default=lambda: str(uuid.uuid4()), primary_key=True)
-    name = db.Column(db.String(), nullable=False)
-    lastname = db.Column(db.String(), nullable=False)
-    nickname = db.Column(db.String(30), nullable=True)
-    codeforces_handle = db.Column(db.String(30), nullable=True)
-    atcoder_handle = db.Column(db.String(30), nullable=True)
-    vjudge_handle = db.Column(db.String(30), nullable=True)
-    fecha_de_nacimiento = db.Column(db.Date(), nullable=True)
+    nickname = db.Column(db.String(30), nullable=True, unique=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    codeforces_handle = db.Column(db.String(30), nullable=True, unique=True)
+    atcoder_handle = db.Column(db.String(30), nullable=True, unique=True)
+    vjudge_handle = db.Column(db.String(30), nullable=True, unique=True)
+    date_of_birth = db.Column(db.Date(), nullable=True, unique=False)
+    image = db.Column(db.String(500), nullable=True, unique=False)
+
     created_at = db.Column(db.DateTime(timezone=True),
                            nullable=False, server_default=db.text("now()"))
     modified_at = db.Column(db.DateTime(timezone=True),
                             nullable=False, server_default=db.text("now()"))
-    miembros = db.relationship('Miembro', backref='usuarios', lazy=True)
+    members = db.relationship('members', backref='users', lazy=True)
+    board = db.relationship('board', backref='users', lazy=True)
 
-    def __init__(self, name, lastname, nickname, fecha_de_nacimiento, codeforces_handle, atcoder_handle, vjudge_handle):
-        self.name = name
-        self.lastname = lastname
+    def __init__(self, nickname, date_of_birth, codeforces_handle, atcoder_handle, vjudge_handle, image):
         self.nickname = nickname
-        self.fecha_de_nacimiento = fecha_de_nacimiento
+        self.date_of_birth = date_of_birth
         self.codeforces_handle = codeforces_handle
         self.atcoder_handle = atcoder_handle
         self.vjudge_handle = vjudge_handle
+        self.image = image
         self.created_at = datetime.utcnow()
         self.modified_at = datetime.utcnow()
 
     def __repr__(self):
-        return f"Usuario {self.name} {self.nickname}"
+        return f"User {self.nickname}"
 
     def serialize(self):
         return {
             'id': self.id,
-            'name': self.name,
-            'lastname': self.lastname,
             'nickname': self.nickname,
+            'email': self.email,
             'codeforces_handle': self.codeforces_handle,
             'atcoder_handle': self.atcoder_handle,
             'vjudge_handle': self.vjudge_handle,
-            'fecha_de_nacimiento': self.fecha_de_nacimiento,
+            'date_of_birth': self.date_of_birth,
             'created_at': self.created_at,
             'modified_at': self.modified_at
         }
 
 
-class Miembro(Usuario):
-    __tablename__ = 'miembros'
+class Member(User):
+    __tablename__ = 'members'
     user_id = db.Column(db.String(36), db.ForeignKey(
-        'usuarios.id'), nullable=False, primary_key=True)
-    miembro_directiva = db.relationship(
-        'Directiva', backref='miembros', lazy=True)
+        'users.id'), nullable=False, primary_key=True)
+    member_since = db.Column(db.DateTime(timezone=True),
+                             nullable=False, server_default=db.text("now()"))
+    comp_status = db.Column(db.Boolean(), default=True, nullable=False)
+    member_status = db.Column(db.Boolean(), default=True, nullable=False)
+
     __mapper_args__ = {
-        'polymorphic_identity': 'miembro',
+        'polymorphic_identity': 'members',
+        'polymorphic_load': 'inline'
     }
 
+    def __init__(self, nickname, date_of_birth, codeforces_handle, atcoder_handle, vjudge_handle, image):
+        super().__init__(nickname, date_of_birth, codeforces_handle,
+                         atcoder_handle, vjudge_handle, image)
+        self.member_since = datetime.utcnow()
 
-class Profesor(db.Model):
-    __tablename__ = 'profesores'
+    def __repr__(self):
+        return f"<Member {self.user_id}>"
+
+
+class Professor(db.Model):
+    __tablename__ = 'professors'
     id = db.Column(db.String(36), nullable=False,
                    default=lambda: str(uuid.uuid4()), primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    apellido = db.Column(db.String(50), nullable=False)
-    correo = db.Column(db.String(100), nullable=False)
-    status = db.Column(db.Boolean, nullable=False)
-    equipo_id = db.Column(db.String(36), db.ForeignKey(
-        'equipos.id'), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+    lastname = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), nullable=False)
+    status = db.Column(db.Boolean(), nullable=False, default=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True),
+                            nullable=False, server_default=db.text("now()"))
+    team_id = db.Column(db.String(36), db.ForeignKey(
+        'teams.id'), nullable=False)
+
+    def __init__(self, name, lastname, email):
+        self.name = name
+        self.lastname = lastname
+        self.email = email
+
+    def __repr__(self):
+        return f"<Professor {self.email}>"
 
 
-profesor_equipo = db.Table('profesor_equipo',
-                           db.Column('profesor_id',  db.String(36), db.ForeignKey(
-                               'profesores.id')),
-                           db.Column('equipo_id', db.String(36), db.ForeignKey('equipos.id')))
+professor_team = db.Table('professor_team',
+                          db.Column('professor_id',  db.String(36), db.ForeignKey(
+                              'professors.id')),
+                          db.Column('team_id', db.String(36), db.ForeignKey('teams.id')))
 
 
-class Equipo(db.Model):
-    __tablename__ = 'equipos'
+class Team(db.Model):
+    __tablename__ = 'teams'
     id = db.Column(db.String(36), nullable=False,
                    default=lambda: str(uuid.uuid4()), primary_key=True)
-    nombre = db.Column(db.String(50), nullable=False)
-    profe_id = db.Column(db.String(36), db.ForeignKey(
-        'profesores.id'), nullable=False)
-    status = db.Column(db.Boolean, nullable=False)
-    profesor = db.relationship(
-        'Profesor', backref='equipos', lazy=True, secondary=profesor_equipo)
+    name = db.Column(db.String(50), nullable=False)
+    professor_id = db.Column(db.String(36), db.ForeignKey(
+        'professors.id'), nullable=False)
+    status = db.Column(db.Boolean(), nullable=False, default=True)
+    professor = db.relationship(
+        'professors', backref='teams', lazy=True, secondary=professor_team)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"<Team {self.name}>"
 
 
 class Contest(db.Model):
     __tablename__ = 'contests'
     id = db.Column(db.String(36), nullable=False,
                    default=lambda: str(uuid.uuid4()), primary_key=True)
-    cantidad_problemas = db.Column(db.Integer, nullable=False)
-    privacidad = db.Column(db.Boolean, nullable=False)
-    nombre = db.Column(db.String(50), nullable=False)
-    duracion = db.Column(db.Integer, nullable=False)
+    num_prob = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    platform = db.Column(db.String(50), nullable=False)
+    link = db.Column(db.String(500), nullable=False)
+
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True),
+                            nullable=False, server_default=db.text("now()"))
+
+    def __init__(self, title, link, platorm, num_prob):
+        self.title = title
+        self.link = link
+        self.platform = platorm
+        self.num_prob = num_prob
+
+    def __repr__(self):
+        return f"<Contest {self.title}>"
 
 
-contest_problema = db.Table('contest_problema', db.Column('contest_id', db.String(36), db.ForeignKey(
-    'contests.id')), db.Column('problema_id', db.String(36), db.ForeignKey('problemas.id')))
+contest_problem = db.Table('contest_problem', db.Column('contest_id', db.String(36), db.ForeignKey(
+    'contests.id')), db.Column('problem_id', db.String(36), db.ForeignKey('problems.id')))
 
 
 class Problema(db.Model):
-    __tablename__ = 'problemas'
+    __tablename__ = 'problems'
     id = db.Column(db.String(36), nullable=False,
                    default=lambda: str(uuid.uuid4()), primary_key=True)
     contest_id = db.Column(db.String(36), db.ForeignKey(
         'contests.id'), nullable=False)
-    link = db.Column(db.String(100), nullable=False)
-    nombre = db.Column(db.String(50), nullable=False)
-    clave = db.Column(db.String(10), nullable=False)
-    plataforma = db.Column(db.String(50), nullable=False)
+    link = db.Column(db.String(500), nullable=False)
+    title = db.Column(db.String(50), nullable=False)
+    platform = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True),
+                            nullable=False, server_default=db.text("now()"))
     contest = db.relationship(
-        'Contest', backref='problemas', lazy=True, secondary=contest_problema)
+        'contests', backref='problems', lazy=True, secondary=contest_problem)
+
+    def __init__(self, title, link, platform):
+        self.title = title
+        self.link = link
+        self.platform = platform
+
+    def __repr__(self):
+        return f"<Problem: {self.title}>"
 
 
-class Directiva(db.Model):
-    __tablename__ = 'directiva'
+class Board(db.Model):
+    __tablename__ = 'board'
     id = db.Column(db.String(36), nullable=False,
                    default=lambda: str(uuid.uuid4()), primary_key=True)
-    cargo = db.Column(db.String(50), nullable=False)
-    fecha_salida_cargo = db.Column(db.DateTime(timezone=True), nullable=True)
-    fecha_entrada_cargo = db.Column(db.DateTime(timezone=True), nullable=False)
+    role = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.Boolean(), default=True, nullable=False)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True),
+                            nullable=False, server_default=db.text("now()"))
     member_id = db.Column(db.String(36), db.ForeignKey(
-        'miembros.user_id'), nullable=True)
+        'users.id'), nullable=True)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'board',
+    }
+
+    def __init__(self, role):
+        self.role = role
+
+    def __repr__(self):
+        return f"<Role: {self.role}>"
+
+
+professor_video = db.Table('professor_video', db.Column('video_id', db.String(36), db.ForeignKey(
+    'videos.id')), db.Column('professor_id', db.String(36), db.ForeignKey('professors.id')))
+
+
+class Video(db.Model):
+    __tablename__ = 'videos'
+    id = db.Column(db.String(36), nullable=False,
+                   default=lambda: str(uuid.uuid4()), primary_key=True)
+    title = db.Column(db.String(60), nullable=False)
+    link = db.Column(db.String(500), nullable=False, unique=True)
+    created_at = db.Column(db.DateTime(timezone=True),
+                           nullable=False, server_default=db.text("now()"))
+    modified_at = db.Column(db.DateTime(timezone=True),
+                            nullable=False, server_default=db.text("now()"))
+    professors = db.relationship(
+        'professors', secondary=professor_video, backref='videos')
+
+    def __init__(self, title, link):
+        self.title = title
+        self.link = link
+
+    def __repr__(self):
+        return f"<Video {self.title}>"
+
 
 # Routes
+
+
 @login_manager.user_loader
 def load_user(user_id):
-    return Usuario.query.get(user_id)
+    return User.query.get(user_id)
+
 
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html')
 
+
 @app.route('/faq', methods=['GET'])
 def faq():
     return render_template('faq.html')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = Usuario.query.filter_by(nickname=request.form['username']).first()
+        user = User.query.filter_by(
+            nickname=request.form['username']).first()
         if user:
             if user.password == request.form['password']:
                 login_user(user)
@@ -195,11 +294,13 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        mail = Usuario.query.filter_by(email=request.form['email']).first()
-        nusername = Usuario.query.filter_by(nickname=request.form['username']).first()
+        mail = User.query.filter_by(email=request.form['email']).first()
+        nusername = User.query.filter_by(
+            nickname=request.form['username']).first()
         if mail and nusername:
             if mail.email == request.form['email']:
                 flash('El correo ya está registrado')
@@ -211,7 +312,7 @@ def signup():
                 flash('Las contraseñas no coinciden')
                 return redirect(url_for('signup'))
             else:
-                user = Usuario(
+                user = User(
                     nickname=request.form['username'],
                     email=request.form['email'],
                     password=request.form['password'],
@@ -229,6 +330,7 @@ def signup():
             return redirect(url_for('signup'))
     else:
         return render_template('signup.html')
+
 
 @app.route('/logout')
 @login_required
