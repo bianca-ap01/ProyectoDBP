7eafe974077d36966dcf8541c26004f317c212eb
bladimirAlfer <124924517+bladimirAlfer@users.noreply.github.com>
2023-05-16 00:11:52 -0500

    Update faq.js

--- static/styles/js/faq.js
+++ static/styles/js/faq.js
@@ -1,16 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const faqItems = document.querySelectorAll(".faq-item");
-
-  faqItems.forEach(function (item) {
-    const question = item.querySelector(".faq-question");
-    const answer = item.querySelector(".faq-answer");
-
-    question.addEventListener("click", function () {
-      if (answer.style.display === 'none' || answer.style.display === '') {
-        answer.style.display = 'block';
-      } else {
-        answer.style.display = 'none';
-      }
+$(document).ready(function() {
+    $('.faq-question').click(function() {
+        var currentAnswer = $(this).next('.faq-answer');
+        $('.faq-answer').not(currentAnswer).slideUp('slow');
+        currentAnswer.slideToggle('slow');
     });
-  });
 });
