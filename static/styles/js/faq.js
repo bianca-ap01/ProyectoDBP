d1a5794a01a9d6be22e9f14b4dbbe012ff7adee9
bladimirAlfer <124924517+bladimirAlfer@users.noreply.github.com>
2023-05-16 21:20:38 -0500

    Update faq.js

--- static/styles/js/faq.js
+++ static/styles/js/faq.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-    $('.faq-question').click(function() {
-        var currentAnswer = $(this).next('.faq-answer');
-        $('.faq-answer').not(currentAnswer).slideUp('slow');
-        currentAnswer.slideToggle('slow');
-    });
+  $('.faq-question').click(function() {
+    var currentQuestion = $(this);
+    var currentAnswer = currentQuestion.next('.faq-answer');
+
+    $('.faq-answer').not(currentAnswer).slideUp('slow');
+    currentAnswer.slideToggle('slow');
+
+    if (currentQuestion.hasClass('expanded')) {
+      currentQuestion.removeClass('expanded');
+    } else {
+      $('.faq-question').removeClass('expanded');
+      currentQuestion.addClass('expanded');
+    }
+  });
 });
