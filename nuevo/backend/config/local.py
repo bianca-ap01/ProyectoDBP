bd5b13595561c1498e6d5908e4067d88685a13fa
bladimirAlfer <bladimir.alferez@utec.edu.pe>
2023-07-06 14:20:22 -0500

    probando

--- nuevo/backend/config/local.py
+++ nuevo/backend/config/local.py
@@ -1,5 +1,5 @@
 config = {
-    'DATABASE_URI': 'postgresql://postgres@localhost:5432/proyectodbp',
+    'DATABASE_URI': 'postgresql://postgres:1234@localhost:5432/proyectodbp',
     'SECRET_KEY': 'prueba123',
     'ALGORYTHM': 'HS256'
 }
