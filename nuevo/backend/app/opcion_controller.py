8747391cf3cbf2b76018fbd7707d686a134e7cc4
bladimirAlfer <124924517+bladimirAlfer@users.noreply.github.com>
2023-07-08 11:26:56 -0500

    controller

--- nuevo/backend/app/opcion_controller.py
+++ nuevo/backend/app/opcion_controller.py
@@ -10,8 +10,8 @@ import jwt
 import datetime
 
 from .models import Opcion
+from .models import db   
 from config.local import config
-
 
 opciones_bp = Blueprint('/opciones', __name__)
 
@@ -19,6 +19,7 @@ opciones_bp = Blueprint('/opciones', __name__)
 def crear_opcion():
     error_list = []
     return_code = 201
+    opcion = None  # Inicializa la opcion como None
 
     try:
         body = request.get_json()
@@ -38,8 +39,12 @@ def crear_opcion():
         if opcion_db is not None :
             if opcion_db.descripcion == descripcion:
                 error_list.append('Ya existe esta opción')
-        else:
+
+        
+        if len(error_list) == 0:
             opcion = Opcion(descripcion=descripcion, id_problema=id_problema)
+            db.session.add(opcion)
+            db.session.commit()
 
     except Exception as e:
         print(e)
@@ -52,8 +57,7 @@ def crear_opcion():
             'error': error_list,
             'message': 'Error creando la opcion'
         }), return_code
-    elif return_code != 201:
-            abort(return_code)
+    elif return_code != 201 or opcion is None:
+        abort(return_code)
     else:
         return jsonify({'id': opcion.id, 'success': True, 'message': 'Opcion creada satisfactoriamente'}), return_code
-
