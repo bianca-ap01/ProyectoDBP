8bc4b592a1de67975caeea742fc7ac43385441ae
bladimirAlfer <124924517+bladimirAlfer@users.noreply.github.com>
2023-07-08 11:30:49 -0500

    Update users_controller

--- nuevo/backend/app/users_controller.py
+++ nuevo/backend/app/users_controller.py
@@ -10,15 +10,17 @@ import jwt
 import datetime
 
 from .models import Usuario
+from .models import db  
 from config.local import config
 
 users_bp = Blueprint('/usuarios', __name__)
-
 
 @users_bp.route('/usuarios', methods = ['POST'])
 def crear_usuario():
     error_list = []
     return_code = 201
+    user = None 
+
     try:
         body = request.get_json()
 
@@ -39,43 +41,42 @@ def crear_usuario():
 
         user_db = Usuario.query.filter(Usuario.username==username).first()
 
-        if user_db is not None :
+        if user_db is not None:
             if user_db.username == username:
                 error_list.append('Ya existe un usuario con este apodo')
-        else:
-            if len(password) < 8:
-                error_list.append('La contraseña debe tener al menos 8 caracteres')
+        
+        if len(password) < 8:
+            error_list.append('La contraseña debe tener al menos 8 caracteres')
 
-            if password != confirmation_password:
-                error_list.append('Las contraseñas ingresadas son diferentes')
+        if password != confirmation_password:
+            error_list.append('Las contraseñas ingresadas son diferentes')
 
-        if len(error_list) > 0:
-            return_code = 400
-        else:
+        # Crear el usuario solo si no hay errores
+        if len(error_list) == 0:
             user = Usuario(username=username, password=password)
-            user_created_id = user.insert()
+            db.session.add(user)
+            db.session.commit()
 
             token = jwt.encode({
-                'user_created_id': user_created_id,
+                'user_created_id': user.id,
                 'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60)
             }, config['SECRET_KEY'], config['ALGORYTHM'])
     except Exception as e:
         print('e: ', e)
+        error_list.append('Error al crear el usuario')
         return_code = 500
 
-    
-    if return_code == 400:
+    if len(error_list) > 0:
         return jsonify({
             'success': False,
             'errors': error_list,
             'message': 'Error creando al nuevo usuario'
-        })
-    elif return_code != 201:
+        }), return_code
+    elif return_code != 201 or user is None:
         abort(return_code)
     else:
         return jsonify({
             'success': True,
             'token': token,
-            'user_created_id': user_created_id,
-        })
-
+            'user_created_id': user.id,
+        }), return_code
